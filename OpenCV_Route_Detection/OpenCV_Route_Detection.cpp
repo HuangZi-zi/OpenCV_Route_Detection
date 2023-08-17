58df760a4548e9ddc3bf4f81c222a6aefeb18a96
HuangZi-zi <rayfilm@foxmail.com>
2023-08-17 11:44:04 +0800

    0817.11:43

--- OpenCV_Route_Detection/OpenCV_Route_Detection.cpp
+++ OpenCV_Route_Detection/OpenCV_Route_Detection.cpp
@@ -17,129 +17,196 @@ using namespace std;
 
 
 class Img {
+private:
+    //导入图片
+    //string path = "Resources/square.png";
+    string path = "Resources/right.png";
+    //导入视频
+    string Vpath = "http://192.168.1.1:8080/?action=stream";
+    
 public:
-    Mat img_1, img_2, img_3;//上、中、下三个判别区域
-    int position_1[2], position_2[2], position_3[2];//左、右标线的中心X座标
+    void import_picture(Mat& origin);
+    void import_video(Mat& origin);
+public:
+    Mat output;
+    Mat img_segment[3];//上、中、下三个判别区域
+    int position[3][2];//左、右标线的中心X座标
+    int status[3];//道路中心座标
+    int flag=0;//有效标识点数目
+    char command = COMM_BRAKE;
 };
+
+void Img::import_picture(Mat& origin)
+{
+    origin = imread(path);
+}
+void Img::import_video(Mat& origin)
+{
+    static VideoCapture cap(Vpath);
+    cap.read(origin);
+}
 
 class ImgProcess: public Img{
 public:
-    Mat kernel_dil = getStructuringElement(MORPH_RECT, Size(5, 5));
-    Mat kernel_erode = getStructuringElement(-MORPH_RECT, Size(9, 9));
-    Mat kernel_canny_dil = getStructuringElement(MORPH_RECT, Size(3, 3));
-
+    //膨胀白色变多
+    Mat kernel_dil_3 = getStructuringElement(MORPH_RECT, Size(3, 3));
+    Mat kernel_dil_5 = getStructuringElement(MORPH_RECT, Size(5, 5));
+    Mat kernel_dil_9 = getStructuringElement(MORPH_RECT, Size(9, 9));
+    //腐蚀黑色变多
+    Mat kernel_erode_3 = getStructuringElement(-MORPH_RECT, Size(3, 3));
+    Mat kernel_erode_9 = getStructuringElement(-MORPH_RECT, Size(9, 9));
 public:
     void Basic_Process(Mat& img);
     void Find_Position();
-
+    void Get_Command();
 };
-
-/*寻找轮廓中心*/
-void ImgProcess::Find_Position()
-{
-    
-    //边缘查找
-    Canny(img_1, img_1, 25, 75);
-    dilate(img_1, img_1, kernel_dil);
-    //轮廓查找
-    vector<vector<Point>> contours;
-    vector<Vec4i> hierachy;
-    findContours(img_1, contours, hierachy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-    int size = contours.size();
-    Moments M;
-    switch (size)
-    {
-    case 0:
-        position_1[0] = 0;
-        position_1[1] = 0;
-        break;
-    case 1:
-        M = moments(contours[0]);
-        position_1[0] = double(M.m10 / M.m00);
-        position_1[1] = 0;
-        break;
-    default:
-        M = moments(contours[0]);
-        position_1[0] = double(M.m10 / M.m00);
-        M = moments(contours[1]);
-        position_1[1] = double(M.m10 / M.m00);
-        break;
-    }
-    cout << "1: " << position_1[0] << "; " << "2: " << position_1[1] << endl;
-}
 
 /*图像基本处理*/
 void ImgProcess::Basic_Process(Mat& img)
 {
     //imshow("origin", img);
-    //添加白色边框
-    //copyMakeBorder(origin, img, 3, 3, 3, 3, BORDER_CONSTANT, Scalar(255, 255, 255));
-    int size = 0;
 
     //灰度
     cvtColor(img, img, COLOR_BGR2GRAY);
     //二值化
     threshold(img, img, 80, 255, cv::THRESH_BINARY);
     //膨胀
-    dilate(img, img, kernel_dil);
+    dilate(img, img, kernel_dil_5);
     //模糊
     GaussianBlur(img, img, Size(9, 9), 3, 3);
     //imshow("blur", img);
     //腐蚀
-    erode(img, img, kernel_erode);
+    erode(img, img, kernel_erode_9);
     //imshow("erode", img);
     //再次二值化
     threshold(img, img, 80, 255, cv::THRESH_BINARY);
-    imshow("Processed", img);
-
+    //imshow("Processed", img);
+    img.copyTo(output);
+    cvtColor(output, output, COLOR_GRAY2BGR);
     //图像切分
-    img_1 = img(Rect(0, 0, img.cols, 40));
-    img_2 = img(Rect(0, 80, img.cols, 40));
-    img_3 = img(Rect(0, 160, img.cols, 40));
-    //imshow("ImageUP", img_1);
-    //imshow("ImageMID", img_2);
-    //imshow("ImageBOT", img_3);
+    Mat temp;
+    for (int i = 0; i < 3; i++)
+    {
+        img(Rect(0, i*80, img.cols, 40)).copyTo(temp);
+            /*注意这里必须使用深拷贝，否则实际上分割后图像仅仅是原图像的新“指针”，会影响后续的处理*/
+        //imshow("temp", temp);
+        copyMakeBorder(temp, img_segment[i], 5, 5, 5, 5, BORDER_CONSTANT, 255);
+    }
+   
+    //imshow("ImageUP", img_segment[0]);
+    //imshow("ImageMID", img_segment[1]);
+    //imshow("ImageBOT", img_segment[2]);
 }
 
-/*遍历像素*/
-class PixelVisit {
-public:
-    int high_pixel = 0;
-    int low_pixel = 0;
-public:
-    void mat_pixel_visit_ptr(Mat& image);
-    PixelVisit() {  }
-    ~PixelVisit() {  }
-};
-
-void PixelVisit::mat_pixel_visit_ptr(Mat& image)
+/*寻找轮廓中心*/
+void ImgProcess::Find_Position()
 {
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierachy;
+    Moments M;
+    for (int i = 0; i < 3; i++)
+    {
+        //边缘查找
+        Canny(img_segment[i], img_segment[i], 25, 75);
+        dilate(img_segment[i], img_segment[i], kernel_dil_3);
+        //轮廓查找
+        findContours(img_segment[i], contours, hierachy, RETR_EXTERNAL, CHAIN_APPROX_NONE, Point());
+        int size = contours.size();
+        //cout << "size of segment " << i << " is " << size << endl;
+        switch (size)
+        {
+        case 0:
+            position[i][0] = 0;
+            position[i][1] = 0;
+            break;
+        case 1:
+            M = moments(contours[0]);
+            position[i][0] = int(M.m10 / M.m00);
+            position[i][1] = 0;
+            flag++;
+            break;
+        default:
+            M = moments(contours[0]);
+            position[i][0] = int(M.m10 / M.m00);
+            M = moments(contours[1]);
+            position[i][1] = int(M.m10 / M.m00);
+            flag++;
+            break;
+        }
+        cout << "1: " << position[i][0] << "; " << "2: " << position[i][1] << endl;
+        status[i] = (position[i][0] + position[i][1]) / 2;
+        //drawContours(img_segment[i], contours, -1, Scalar(0, 255, 0), 2);
+        circle(output, Point2d(position[i][0], i * 80 + 20), 1, Scalar(255, 0, 255), 2, 8, 0);
+        circle(output, Point2d(position[i][1], i * 80 + 20), 1, Scalar(255, 0, 255), 2, 8, 0);
+
+    }
+    //imshow("up", img_segment[0]);
+    //imshow("mid", img_segment[1]);
+    //imshow("bot", img_segment[2]);
+    imshow("output", output);
     
+}
+
+/*判断控制指令*/
+void ImgProcess::Get_Command()
+{
+    switch (flag)
+    {
+    case 0:
+    case 1:
+        command = COMM_BACK;
+        cout << "BACK" << endl;
+        break;
+    case 2:
+    default:
+        break;
+    }
+    int left = 0, right = 0;
+    //left = position[0][0] + position[1][0] + position[2][0];
+    //right = position[0][1] + position[1][1] + position[2][1];
+    for (int i = 0; i < 3; i++)
+    {
+        if (status[i])
+        {
+        }
+    }
+    if (status[0]==0&&status[1] == 0)
+    {
+        
+    }
+
+    if (command == COMM_BRAKE)
+    {
+        if (position[0][0] - position[2][0] > 0)
+        {
+            command = COMM_RIGHT;
+            cout << "RIGHT" << endl;
+        }
+        else
+        {
+            command = COMM_LEFT;
+            cout << "LEFT" << endl;
+        }
+    }
+    
+    command = COMM_FORWARD;
+    cout << "FORWARD" << endl;
+
 }
 
 
 
 int main()
 {
-    PixelVisit pixelvisit;
     ImgProcess IMGPROCESS;
     Img IMG;
     Mat origin, imgCanny;
-    
-    //导入图片
-    //string path = "Resources/square.png";
-    string path = "Resources/forward.png";
-
-    //导入视频
-    //string Vpath = "http://192.168.1.1:8080/?action=stream";
-    //VideoCapture cap(Vpath);
-
 
     int size;
     while(1)
     {
-        origin = imread(path);
-        //cap.read(origin);
+        IMG.import_picture(origin);
+        //IMG.import_video(origin);
         if (origin.empty())
         {
             cout << "could not load image...." << endl;
@@ -148,28 +215,7 @@ int main()
         IMGPROCESS.Basic_Process(origin);
         IMGPROCESS.Find_Position();
 
-        //if (size >= 2)
-        //{
-        //    //前进
-        //    cout << "forward" << endl;
-        //    Send(COMM_FORWARD);
-        //}
-        //else
-        //{
-
-            //qd.mat_pixel_visit_ptr(img_1);
-            //if (qd.high_pixel > qd.low_pixel)
-            //{
-            //    //右转
-            //    cout << "right" << endl;
-            //    Send(COMM_RIGHT);
-            //}
-            //else
-            //{
-            //    //左转
-            //    cout << "left" << endl;
-            //    Send(COMM_LEFT);
-            //}
+        //Send(IMG.command);
         
 
         waitKey(0);
